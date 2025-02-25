5d0710bd9175a0b43e4812936c20a8226e58dc33
Ravikumar Chennupati <rkchowdary2714@gmail.com>
2025-02-25 16:34:29 -0600

    Student Performance Python file

--- stu_per.py
+++ stu_per.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler,LabelEncoder
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://Ravi:rk123@cluster0.thirf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://XXXX:XXXXX@cluster0.thirf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
